a850059290e7cf6c8533b4b01f4645e0900f35cc
Lealem Birhanu <lealembirhanu2000@gmail.com>
2022-05-14 15:33:17 +0300

    projects when learning vuetify

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About vuetify todo app</h1>
+
+    <p>Phasellus vitae sem id mi rutrum tristique. Curabitur aliquet sem vitae elit aliquam ornare. Aliquam eget risus id est vulputate malesuada ut id libero. Maecenas sit amet dignissim mauris. In ac viverra justo, congue rutrum augue. Mauris sagittis interdum finibus. Duis ut sollicitudin orci. Vivamus pharetra mauris dolor, in facilisis eros porta at. Phasellus eu semper leo. Mauris a blandit lorem. Ut in laoreet augue. Praesent in malesuada lectus. Ut in urna vel nisi lobortis consequat</p>
+
+    <p>Phasellus vitae sem id mi rutrum tristique. Curabitur aliquet sem vitae elit aliquam ornare. Aliquam eget risus id est vulputate malesuada ut id libero. Maecenas sit amet dignissim mauris. In ac viverra justo, congue rutrum augue. Mauris sagittis interdum finibus. Duis ut sollicitudin orci. Vivamus pharetra mauris dolor, in facilisis eros porta at. Phasellus eu semper leo. Mauris a blandit lorem. Ut in laoreet augue. Praesent in malesuada lectus. Ut in urna vel nisi lobortis consequat</p>
+
+    <p>Phasellus vitae sem id mi rutrum tristique. Curabitur aliquet sem vitae elit aliquam ornare. Aliquam eget risus id est vulputate malesuada ut id libero. Maecenas sit amet dignissim mauris. In ac viverra justo, congue rutrum augue. Mauris sagittis interdum finibus. Duis ut sollicitudin orci. Vivamus pharetra mauris dolor, in facilisis eros porta at. Phasellus eu semper leo. Mauris a blandit lorem. Ut in laoreet augue. Praesent in malesuada lectus. Ut in urna vel nisi lobortis consequat</p>
+
+    <p>Phasellus vitae sem id mi rutrum tristique. Curabitur aliquet sem vitae elit aliquam ornare. Aliquam eget risus id est vulputate malesuada ut id libero. Maecenas sit amet dignissim mauris. In ac viverra justo, congue rutrum augue. Mauris sagittis interdum finibus. Duis ut sollicitudin orci. Vivamus pharetra mauris dolor, in facilisis eros porta at. Phasellus eu semper leo. Mauris a blandit lorem. Ut in laoreet augue. Praesent in malesuada lectus. Ut in urna vel nisi lobortis consequat</p>
   </div>
 </template>
